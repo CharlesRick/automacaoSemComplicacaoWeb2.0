53934a737a9b0ade2bbc7ae32e029a01bef1e2f6
CharlesOliveira <charles.oliveira_noesis@prestador.globo>
2022-05-21 19:03:48 -0300

    aula 14 runner e steps

---
+++ src/test/java/br/com/chronosAcademy/steps/LoginSteps.java
@@ -0,0 +1,62 @@
+package br.com.chronosAcademy.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+
+    @Dado("que a modal esteja sendo exibida")
+    public void queAModalEstejaSendoExibida() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("for realizado um clique no icone de fechar")
+    public void forRealizadoUmCliqueNoIconeDeFechar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Entao("a janela model deve ser fechada")
+    public void aJanelaModelDeveSerFechada() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("for realizado um clique fora da modal")
+    public void forRealizadoUmCliqueForaDaModal() {
+    }
+
+    @Quando("for realizado no clique no link create new account")
+    public void forRealizadoNoCliqueNoLinkCreateNewAccount() {
+
+    }
+
+    @Entao("a pagina create new account deve ser exibida")
+    public void aPaginaCreateNewAccountDeveSerExibida() {
+
+    }
+
+    @Quando("os campos de login forem preenchido da seguinte forma")
+    public void osCamposDeLoginForemPreenchidoDaSeguinteForma() {
+
+    }
+
+    @Quando("for realizado um clique no botao sign in")
+    public void forRealizadoUmCliqueNoBotaoSignIn() {
+
+    }
+
+    @Entao("deve ser possivel logar no sistema")
+    public void deveSerPossivelLogarNoSistema() {
+
+    }
+
+    @Entao("o sistema deve exibir uma mensagem de erro")
+    public void oSistemaDeveExibirUmaMensagemDeErro() {
+
+    }
+
+    @Entao("o botao sign in deve permanecer desabilitado")
+    public void oBotaoSignInDevePermanecerDesabilitado() {
+    }
+}
